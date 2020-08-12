cd771053180a507b302350e911133d894e55e501
vb <vbacik.10@gmail.com>
2020-08-12 11:29:13 +0300

    magic number converted

--- login.dart
+++ login.dart
@@ -2,9 +2,13 @@ class LoginModel {
   String user;
   String password;
 
+  int loginValidateLength = 0;
+
   void login() {
-    if (user.length > 6 && password.length > 6) {
+    if (user.length > LOGIN_VALIDATE_LENGTH && password.length > LOGIN_VALIDATE_LENGTH) {
       print("oke");
     }
   }
 }
+
+const LOGIN_VALIDATE_LENGTH = 6;
