40949f173ddb79360822dab150c703b2a9e9a964
vb <vbacik.10@gmail.com>
2020-08-12 11:27:54 +0300

    user lenght validation change to six

--- login.dart
+++ login.dart
@@ -3,7 +3,7 @@ class LoginModel {
   String password;
 
   void login() {
-    if (user.length > 5 && password.length > 6) {
+    if (user.length > 6 && password.length > 6) {
       print("oke");
     }
   }
